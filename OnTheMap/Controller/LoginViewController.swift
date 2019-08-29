fbfb547511df981e7187a1c29b4eeb1ad7066ded
Charlie Scheer <scheer.charlie@gmail.com>
2019-08-28 17:06:47 -0700

    Initial commit

---
+++ OnTheMap/Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Charlie Scheer on 8/28/19.
+//  Copyright © 2019 Praxsis. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
