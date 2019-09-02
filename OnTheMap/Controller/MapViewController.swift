8e215a8de32682ee0f6503dd36c79966e21cd647
Charlie Scheer <scheer.charlie@gmail.com>
2019-09-01 17:25:51 -0700

    Added ability to see pins from fetched student locations in Mapview

--- OnTheMap/Controller/MapViewController.swift
+++ OnTheMap/Controller/MapViewController.swift
@@ -7,15 +7,55 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: MapAndPinViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        handleStudentLocationResponse()
         print("Map View")
+        
+        
     }
     
-    
-    @IBAction func tempButtonWasTapped(_ sender: Any) {
-        OnTheMapAPIClient.getLoggedInUserData(url: OnTheMapAPIClient.Endpoints.getUserData.url, userId: OnTheMapAPIClient.Auth.key)
+    func handleStudentLocationResponse() {
+        startActivityIndicator(activityIndicator, true)
+        
+        
+        OnTheMapAPIClient.getStudentLocations { (success, results, error) in
+            if success {
+                if let studentLocationArray = results {
+                    self.studentLocationResults = studentLocationArray
+                    self.addPinsFromStudentArrayToMap()
+                    self.startActivityIndicator(self.activityIndicator, false)
+                }
+            } else {
+                print(error!.localizedDescription)
+            }
+        }
+        
     }
+    
+    func addPinsFromStudentArrayToMap() {
+        for student in studentLocationResults {
+            createPinForStudent(student: student)
+        }
+    }
+    
+    func createPinForStudent(student: StudentLocationResults) {
+        let annotation = MKPointAnnotation()
+        let latitude = CLLocationDegrees(exactly: Float(student.latitude))
+        let longitude = CLLocationDegrees(exactly: Float(student.longitude))
+        let coord = CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
+        
+        annotation.coordinate = coord
+        annotation.title = student.firstName + " " + student.lastName
+        
+        mapView.addAnnotation(annotation)
+    }
+
 }
